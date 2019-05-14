0ea4c2336fb27c7767ce89e58124626690233a5f
Patrick Gordon <patrickgordon42@yahoo.com>
2019-05-14 13:14:41 -0500

    did some of the simple event lsiteners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+// mouseover 'Fun Bus' (first h1), change background color to dodgerblue
+let name = document.querySelector('h1');
+name.addEventListener('mouseover', a => {
+    document.body.style.backgroundColor = 'dodgerBlue';
+})
+
+//click body anywhere, changes text color to purple
+let bodyclick = document.querySelector("body");
+bodyclick.addEventListener("click", a => {
+    document.body.style.color = "purple";
+})
+
+// double click bus image to change background color to limegreen
+let doubleClick = document.querySelector('img');
+doubleClick.addEventListener('dblclick', a => {
+    document.body.style.backgroundColor = 'limegreen'
+})
